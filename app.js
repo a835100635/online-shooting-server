8f3b96e68a1f2664808f83fa87916f9ff7f9e1e8
leonli <leonli@wezhuiyi.com>
2022-11-24 18:01:47 +0800

    feat: scoket广播

--- app.js
+++ app.js
@@ -24,14 +24,29 @@ io.on("connection", client => {
   client.nsp.sockets.forEach(element => {
     ids.push(element.id);
   });
-  console.log("总连接 ids --->", JSON.stringify(ids));
+  console.log("总连接 ids --->", JSON.stringify(ids), allClient);
 
+  // 接收到的玩家信息
   client.on('update_player', data => {
     console.log('player 更新 --->', JSON.stringify(data));
     allClient.set(data.sid, data);
 
-    client.emit('_update', dataMap());
+    // 广播
+    client.broadcast.emit('_update', dataMap());
   })
+
+  // 接收到的子弹信息
+  client.on('update_bullet', data => {
+    console.log('bullet --->', JSON.stringify(data));
+    // 广播
+    client.broadcast.emit('_update_bullet', JSON.parse(JSON.stringify(data)));
+  })
+
+  client.on('delete_bullet', bulletId => {
+    console.log('delete_bullet --->', bulletId);
+    // 广播
+    client.broadcast.emit('_delete_bullet', bulletId);
+  });
 
   // 断开连接
   client.on('disconnect', data => {
@@ -39,8 +54,9 @@ io.on("connection", client => {
 
     // 删除断开连接的玩家
     allClient.delete(client.id);
-    
-    client.emit('_update', dataMap());
+
+    // 广播
+    client.broadcast.emit('_update', dataMap());
   });
 });
 
@@ -48,6 +64,6 @@ io.on("connection", client => {
 
 
 let ws = server.listen(3000, function () {
-    console.log('start at port: /*ip*/ ' + ws.address().port);
+    console.log('start at port: localhost:' + ws.address().port);
 });
 
